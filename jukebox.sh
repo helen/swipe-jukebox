dc661dfffe60741369b3386860dafe9e5376f783
Helen Hou-Sandi <helen.y.hou@gmail.com>
2019-12-22 12:22:49 -0600

    Collapse mpc commands down to one line

--- jukebox.sh
+++ jukebox.sh
@@ -3,10 +3,8 @@
 echo "Jukebox started!"
 
 while read -ep "Swipe: " INPUT; do
+	# Swipe input contains track delimiters, let's extract just the first number and use it
 	read -a ID <<< "${INPUT//[^0-9]/ }"
 	URI=$( head -"$ID" songs.txt | tail -1 )
-	mpc stop
-	mpc clear
-	mpc add "$URI"
-	mpc play
+	mpc stop && mpc clear && mpc add "$URI" && mpc play
 done
