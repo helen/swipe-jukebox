dec34d9683f946e25500e6904b879863e3f96448
Helen Hou-Sandi <helen.y.hou@gmail.com>
2019-12-22 12:58:24 -0600

    Use `sed` and check variables before continuing

--- jukebox.sh
+++ jukebox.sh
@@ -5,6 +5,16 @@ echo "Jukebox started!"
 while read -ep "Swipe: " INPUT; do
 	# Swipe input contains track delimiters, let's extract just the first number and use it
 	read -a ID <<< "${INPUT//[^0-9]/ }"
-	URI=$( head -"$ID" songs.txt | tail -1 )
+
+	if [[ -z "$ID" ]]; then
+		continue
+	fi
+
+	URI=$( sed "${ID}q;d" songs.txt )
+
+	if [[ -z "$URI" ]]; then
+		continue
+	fi
+
 	mpc stop && mpc clear && mpc add "$URI" && mpc play
 done
